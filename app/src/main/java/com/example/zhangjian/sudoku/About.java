96e71537e87c0a8c9ae2cae4e08fea97c35afe80
Zhang Jian <zhangjian19921022@163.com>
2015-04-23 19:06:21 +0800

    Master stream finished About activity

--- app/src/main/java/com/example/zhangjian/sudoku/About.java
+++ app/src/main/java/com/example/zhangjian/sudoku/About.java
@@ -9,20 +9,10 @@ import android.view.View.OnClickListener;
 /**
  * Created by zhangjian on 2015/4/22.
  */
-public class About extends ActionBarActivity implements OnClickListener{
+public class About extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
 
-        View backButton = this.findViewById(R.id.back_from_about);
-        backButton.setOnClickListener(this);
-    }
-
-    public void onClick(View v){
-        switch (v.getId()) {
-            case R.id.back_from_about:
-                this.finish();
-                break;
-        }
     }
 }
